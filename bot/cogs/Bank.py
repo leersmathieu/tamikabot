3d7d5c99733e0998c7d527b1901b4c675c8397e9
leersmathieu <leers.mathieu@gmail.com>
2021-08-26 19:09:11 +0200

    upgrade bank cogs

--- bot/cogs/Bank.py
+++ bot/cogs/Bank.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 import re
 
+
 class Bank(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,36 +14,35 @@ class Bank(commands.Cog):
 
     @commands.command(name='add_coins', pass_context=True)
     async def add_coins(self, ctx: Context, user, amount):
-        # Note for *, it tells the library to put everything
-        # the user types after it into message as a string.
+
+        user = str(re.findall(r'\b\d+\b', user)[0])
+        user_mention = await self.bot.fetch_user(int(user))
 
         if ctx.message.author.id == self.tamikara_id:
-
-            user = re.findall(r'\b\d+\b', user)
-            user = str(user[0])
-            print(user)
-            print(type(user))
-            user_mention = await self.bot.fetch_user(int(user))
-
             try:
                 if user in self.db.index.tolist():
-                    print("i exist")
                     self.db.at[user, 'bank'] += int(amount)
-                    # self.db[self.db["discord_id"] == ctx.author.id]["bank"] += int(amount)
+
                 else:
-                    print("i dont exist")
                     self.db = self.db.append(pd.Series({'bank': int(amount)}, name=user))
 
-                print(self.db)
+                # print(self.db)
                 with open('./bot/db/filename.pickle', 'wb') as handle:
                     pickle.dump(self.db, handle)
 
                 await ctx.send(f"{user_mention.mention} gagne {amount} coins !")
 
-
             except Exception as error:
                 print(error)
         else:
-            await ctx.send('not authorized')
-        print("end")
+            await ctx.send(f"{user_mention.mention} dont try to break the rules!")
 
+    @commands.command(name='bank', pass_context=True)
+    async def bank(self, ctx: Context):
+
+        try:
+            await ctx.send(f"Tu as {self.db.loc[str(ctx.author.id), 'bank']} coins")
+
+        except Exception as error:
+            print(f"error: {error}")
+            await ctx.send("Tu n'as pas de compte en banque !")
